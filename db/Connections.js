7707a68c47a04fdf075f480b59a0c9fedb551838
Kumarswammy <kumarswamyshinagam8@gmail.com>
2025-04-03 15:16:24 +0530

    Third commit

--- db/Connections.js
+++ db/Connections.js
@@ -1,10 +1,24 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/finalproject")
+
+// 	.then(() => {
+// 		console.log("Connected to DB Successfully!!!")
+// 	}).catch((err) => {
+// 		console.log(err)
+// 	})
+
+require('dotenv').config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/finalproject")
+console.log(process.env.MONGO_URI)
 
-	.then(() => {
-		console.log("Connected to DB Successfully!!!")
-	}).catch((err) => {
-		console.log(err)
-	})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB Atlas Successfully!");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
 
+module.exports = mongoose;  // 👈 Export mongoose connection
